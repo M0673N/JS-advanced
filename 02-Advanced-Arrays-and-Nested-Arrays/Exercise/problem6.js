2d37987f422034396e0814abec45282975436e79
Stoyan Valkov <m0673n@abv.bg>
2021-09-12 01:34:49 +0300

    Renamed folder.

--- 02-Advanced-Arrays-and-Nested-Arrays/Exercise/problem6.js
+++
@@ -1,9 +0,0 @@
-function solve(array) {
-    let result = '';
-    array.sort((a, b) => a.localeCompare(b));
-    for (let index = 0; index < array.length; index++) {
-        result += `${index + 1}.${array[index]}\n`;
-    }
-    return result.trimRight();
-}
-// console.log(solve(["John", "Bob", "Christina", "Ema"]))
